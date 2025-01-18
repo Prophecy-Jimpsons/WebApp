aee65c4dfd9e2d51d7d86286d5d88e46fc2682c3
mrarejimmyz <mrarejimmy@icloud.com>
2025-01-18 15:24:53 -0500

    added certificate to make it https

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 // App Constant
 // config/constants.ts
 export const SERVER_BASE_URL =
-  import.meta.env.VITE_SERVER_BASE_URL || "http://173.34.178.13:8000";
+  import.meta.env.VITE_SERVER_BASE_URL || "https://173.34.178.13:8000";
