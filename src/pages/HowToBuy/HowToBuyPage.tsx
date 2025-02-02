5adfb34e30b02bb8f2a18920221e150368e5d2e4
blessedrab <blessedrab@proton.me>
2025-02-01 19:37:21 -0500

    resolve how-to-buy for build issue

--- src/pages/HowToBuy/HowToBuyPage.tsx
+++ src/pages/HowToBuy/HowToBuyPage.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
 
 import MainLayout from "@/layouts/MainLayout/MainLayout";
-import BuyStep from "@/components/composite/BuySteps";
-import BuyingSteps from "@/components/composite/BuySteps/BuyingSteps";
+// import BuyStep from "@/components/composite/BuySteps";
+import BuyingSteps from "@/components/composite/BuySteps";
 
 const HowToBuyPage: FC = () => {
   return (
