7dcb38cd3c9eb0d742d0eca94579f35c24a6b311
mrarejimmyz <mrarejimmy@icloud.com>
2025-02-05 14:12:02 -0500

    syndica api log added

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -10,7 +10,7 @@ import { useQuery } from "@tanstack/react-query";
 const syndicaKey = import.meta.env.SYNDICA_API;
 const syndicaHostUrl = `https://solana-mainnet.api.syndica.io/api-key/${syndicaKey}`;
 
-console.log("Syndica API Key:", import.meta.env.VITE_SYNDICA_API);
+console.log("Syndica API Key:", import.meta.env.SYNDICA_API);
 console.log("Syndica Host URL:", syndicaHostUrl);
 
 
